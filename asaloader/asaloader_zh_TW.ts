cd62592724712adc4ed95a6f556b32ab7bbb8d3f
void110916 <110916fong@gmail.com>
2022-08-17 16:35:28 +0800

    Add language translate file

--- asaloader/asaloader_zh_TW.ts
+++ asaloader/asaloader_zh_TW.ts
@@ -1,3 +1,93 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_TW"></TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="loader/asadevice.h" line="17"/>
+        <source>auto</source>
+        <translation>自動偵測</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="22"/>
+        <source>Default, auto detect device type.</source>
+        <translation>預設，自動偵測裝置類型</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="24"/>
+        <source>asa_m128_v1</source>
+        <translation>asa_m128_v1</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="31"/>
+        <source>asa_m128_v2</source>
+        <translation>asa_m128_v2</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="38"/>
+        <source>asa_m128_v3</source>
+        <translation>asa_m128_v3</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="45"/>
+        <source>asa_m3_v1</source>
+        <translation>asa_m3_v1</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="52"/>
+        <source>asa_m4_v1</source>
+        <translation>asa_m4_v1</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="43"/>
+        <source>Available device list:</source>
+        <translation>可用裝置列表:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="44"/>
+        <source>    device name   	 num 	 note</source>
+        <translation>    裝置名稱    \t\t 編號 \t 備註</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="46"/>
+        <source>  - %1  	 %2	 %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="90"/>
+        <source>Device is </source>
+        <translation>裝置: </translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="92"/>
+        <source>Flash hex size is %1 KB (%2 bytes)</source>
+        <translation>FLASH 大小為 %1 KB (%2 位元組)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="97"/>
+        <source>Externel Flash hex size is %1 KB (%2 bytes)</source>
+        <translation>外部 Flash 大小為 %1 KB (%2 位元組)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="102"/>
+        <source>EEPROM hex size is %1 bytes.</source>
+        <translation>使用EEPROM大小為 %1 位元組</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="105"/>
+        <source>Estimated time is %1 s.</source>
+        <translation>估計時間是 %1 s</translation>
+    </message>
+    <message>
+        <location filename="loader/prog_arg.h" line="47"/>
+        <location filename="loader/prog_arg.h" line="63"/>
+        <source>No Device detect</source>
+        <translation>未偵測到燒錄裝置</translation>
+    </message>
+    <message>
+        <location filename="loader/prog_arg.h" line="77"/>
+        <source>No File exit</source>
+        <translation>未偵測到燒錄檔案</translation>
+    </message>
+</context>
+</TS>
