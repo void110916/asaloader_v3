8ed575b48a3e8618f0f6b5b9c8c5bff7288f2e66
void110916 <110916fong@gmail.com>
2022-09-01 00:25:45 +0800

    Add en US translate file

---
+++ asaloader/asaloader_en_US.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="loader/asadevice.h" line="17"/>
+        <source>auto</source>
+        <translation>auto</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="22"/>
+        <source>Default, auto detect device type.</source>
+        <translation>Default, auto detect device type.</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="24"/>
+        <source>asa_m128_v1</source>
+        <translation>asa_m128_v1</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="31"/>
+        <source>asa_m128_v2</source>
+        <translation>asa_m128_v2</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="38"/>
+        <source>asa_m128_v3</source>
+        <translation>asa_m128_v3</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="45"/>
+        <source>asa_m3_v1</source>
+        <translation>asa_m3_v1</translation>
+    </message>
+    <message>
+        <location filename="loader/asadevice.h" line="52"/>
+        <source>asa_m4_v1</source>
+        <translation>asa_m4_v1</translation>
+    </message>
+    <message>
+        <location filename="loader/prog_arg.h" line="47"/>
+        <location filename="loader/prog_arg.h" line="63"/>
+        <source>No Device detect</source>
+        <translation>No Device detect</translation>
+    </message>
+    <message>
+        <location filename="loader/prog_arg.h" line="77"/>
+        <source>No File exit</source>
+        <translation>No File exit</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="43"/>
+        <source>Available device list:</source>
+        <translation>Available device list:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="44"/>
+        <source>    device name   	 num 	 note</source>
+        <translation>    device name   	 num 	 note</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="46"/>
+        <source>  - %1  	 %2	 %3</source>
+        <translation>  - %1  	 %2	 %3</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="90"/>
+        <source>Device is </source>
+        <translation>Device is </translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="92"/>
+        <source>Flash hex size is %1 KB (%2 bytes)</source>
+        <translation>Flash hex size is %1 KB (%2 bytes)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="97"/>
+        <source>Externel Flash hex size is %1 KB (%2 bytes)</source>
+        <translation>Externel Flash hex size is %1 KB (%2 bytes)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="102"/>
+        <source>EEPROM hex size is %1 bytes.</source>
+        <translation>EEPROM hex size is %1 bytes.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="105"/>
+        <source>Estimated time is %1 s.</source>
+        <translation>Estimated time is %1 s.</translation>
+    </message>
+</context>
+</TS>
